983b215a2463c8e208aefff603921de5b679bfed
Steve <steve.shortt@gmail.com>
2018-03-30 21:54:32 -0500

    quick commit

--- utility.Server.Extensibility/Classes/ApiMethod.cs
+++ utility.Server.Extensibility/Classes/ApiMethod.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 
@@ -12,6 +14,8 @@ namespace Synapse.Server.Extensibility.Utility
         public string Route { get; set; }
         public string PlanName { get; set; }
         public string CodeBlob { get; set; }
+        public ApiMethodOptions Options { get; set; }
+        bool HasOptions { get { return Options != null; } }
 
         public string ToClassCode()
         {
@@ -19,7 +23,7 @@ namespace Synapse.Server.Extensibility.Utility
         [Route( ~~Route~~ )]
         public ~~ReturnType~~ ~~Name~~()
         {
-            ~~CodeBlob~~
+            ~~CodeBlob~~~~plan~~
         }
 ";
 
@@ -30,11 +34,22 @@ namespace Synapse.Server.Extensibility.Utility
             if( string.IsNullOrWhiteSpace( ReturnType ) )
                 ReturnType = "string";
 
+            string plan = string.Empty;
+            if( !string.IsNullOrWhiteSpace( PlanName ) )
+            {
+                plan = @"return (~~ReturnType~~)CallPlan( planUniqueName: ~~planUniqueName~~~~options~~);";
+                plan = Regex.Replace( plan, "~~planUniqueName~~", $"{PlanName}" );
+                plan = Regex.Replace( plan, "~~options~~", HasOptions ? Options.ToString() : null );
+                if( !string.IsNullOrWhiteSpace( CodeBlob ) )
+                    plan = "\r\n\r\n            " + plan;
+            }
+            code = Regex.Replace( code, "~~plan~~", plan );
+
             code = Regex.Replace( code, "~~HttpMethod~~", $"{HttpMethod}" );
             code = Regex.Replace( code, "~~Route~~", $"\"{Route}\"" );
             code = Regex.Replace( code, "~~ReturnType~~", ReturnType );
             code = Regex.Replace( code, "~~Name~~", Name );
-            code = Regex.Replace( code, "~~CodeBlob~~", CodeBlob );
+            code = Regex.Replace( code, "~~CodeBlob~~", !string.IsNullOrWhiteSpace( CodeBlob ) ? CodeBlob : string.Empty );
 
             return code;
         }
@@ -55,10 +70,42 @@ namespace Synapse.Server.Extensibility.Utility
             {
                 Name = "MyCustomMethod",
                 Route = "custom/path",
-                CodeBlob = "return \"foo\";"
+                PlanName = "SomethingWonderful",
+                CodeBlob = "string foo = \"foo\";",
+                Options = new ApiMethodOptions
+                {
+                    SerializationType = SerializationType.Html,
+                    TimeoutSeconds = 10,
+                    PollingIntervalSeconds = 2
+                }
             };
 
             return sample;
+        }
+    }
+
+    public class ApiMethodOptions
+    {
+        public string Path { get; set; } = "Actions[0]:Result:ExitData";
+        public SerializationType SerializationType { get; set; } = SerializationType.Json;
+        public bool SetContentType { get; set; } = true;
+        public int PollingIntervalSeconds { get; set; } = 1;
+        public int TimeoutSeconds { get; set; } = 120;
+        public string NodeRootUrl { get; set; } = null;
+
+        public override string ToString()
+        {
+            List<string> v = new List<string>();
+            string path = !string.IsNullOrWhiteSpace( Path ) && Path != "Actions[0]:Result:ExitData" ? $"path: \"{Path}\"" : null;
+            string st = SerializationType != SerializationType.Json ? $"serializationType: {SerializationType}" : null;
+            string sct = SetContentType ? null : $"setContentType: {SetContentType}";
+            string pi = PollingIntervalSeconds == 1 ? null : $"pollingIntervalSeconds: {PollingIntervalSeconds}";
+            string ts = TimeoutSeconds == 120 ? null : $"timeoutSeconds: {TimeoutSeconds}";
+            string nru = !string.IsNullOrWhiteSpace( NodeRootUrl ) ? $"nodeRootUrl: \"{NodeRootUrl}\"" : null;
+
+            string vars = ", " + string.Join( ", ", new string[] { path, st, sct, pi, ts, nru } ) + " ";
+
+            return vars;
         }
     }
 }
---
+++ utility.Server.Extensibility/Classes/Enums/SerializationType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Synapse.Server.Extensibility.Utility
+{
+    public enum SerializationType
+    {
+        Yaml = 0,
+        Xml = 1,
+        Json = 2,
+        Html = 3,
+        Unspecified = 4
+    }
+}
